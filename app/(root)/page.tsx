94a8844527561d1c6f04b737e947f422a56abf11
Kantamas <pokkam55@gmail.com>
2023-12-29 03:14:20 +0700

    Update layout and config tailwind css

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+import clsx from "clsx";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <section>
+        <div className={clsx([`container`])}>
+          <p>Hello</p>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default HomePage;
---
+++ components/UI/Button/index.tsx
@@ -0,0 +1,42 @@
+import React, { MouseEvent } from "react";
+
+import clsx from "clsx";
+
+import { ButtonProps } from "./types";
+
+const Button = ({
+  variant,
+  size = "default",
+  rounder = "default",
+  className,
+  children,
+  active,
+  loading,
+  isOutline,
+  isInvert,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={clsx(
+        `button`,
+        [`button-rounder-${rounder}`],
+        [`button-type-${size}`],
+        {
+          "button-loading": loading,
+          active: active,
+          "button-outline": isOutline,
+          "button-invert": isInvert,
+        },
+        variant,
+        `focus:outline-none disabled:cursor-not-allowed disabled:opacity-30`,
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
